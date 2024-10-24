84721dc7e7e1a161973a3232e05a5955112646c1
Anna <anyalar04@gmail.com>
2024-10-24 19:40:57 +0300

    feat: intro section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
+import IntroSection from './components/IntroSection/IntroSection';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        {/* <Intro />
-        <Projects /> */}
+        <IntroSection />
+        {/* <Projects /> */}
       </main>
     </div>
   );
---
+++ src/components/IntroSection/IntroSection.js
@@ -0,0 +1,20 @@
+import styles from './IntroSection.module.css';
+
+function IntroSection() {
+  return (
+    <section className={styles.intro}>
+      <div className={'container'}>
+        <h1 className={styles.introTitle}>Projects</h1>
+        <p className={styles.introDescription}>
+          From configuration to security, web apps to big data—whatever the
+          infrastructure needs of your application may be, there is a Spring
+          Project to help you build it. Start small and use just what you
+          need—Spring is modular by design.
+        </p>
+        <button className={styles.introButton}>Release Calendar</button>
+      </div>
+    </section>
+  );
+}
+
+export default IntroSection;
