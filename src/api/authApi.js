7bdcff3be248cf7772a03a1b2a7eca2cec91a3d8
Anna <anyalar04@gmail.com>
2024-11-15 16:54:32 +0300

    refactor: remove returning body

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,15 +4,10 @@ import { HTTP_STATUS_CODES } from '../utils/httpStatusCodes';
 
 export const authLogin = async ({ username, password }) => {
   try {
-    const response = await axios.post(
-      `${process.env.REACT_APP_BACKEND_URL}/login`,
-      {
-        username,
-        password,
-      },
-    );
-
-    return response.data.isAuthenticated;
+    await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+      username,
+      password,
+    });
   } catch (error) {
     if (
       error.response &&
